dcb24f7c27ef4e5b92081388f1fb7da3ffc3d8ee
home <home@homes-Mac-mini.local>
2017-05-30 00:39:27 -0400

    bug fixed

--- asst2/var_eli.py
+++ asst2/var_eli.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 
 class Factor(object):
@@ -9,27 +10,28 @@ class Factor(object):
 
 def restrict(factor, variable, value):
     if len(factor.var) < 2:
-        return factor
+        return False
     if variable in factor.var:
         index = factor.var.index(variable)
         factor.var.remove(variable)
         x = 0 if value else 1
-        slc = [slice(None)] * len(factor.var)
-        slc[index] = x
-        return Factor(factor.array[slc], factor.var)
+        return_src = 'Factor(factor.array[%s %d, ...], factor.var)' % (':,' * index, x)
+        return eval(return_src)
 
 
 def multiply(f1, f2):
-    var_list = list(f1.var)
-    for v in f2.var:
+    x1 = copy.deepcopy(f1)
+    x2 = copy.deepcopy(f2)
+    var_list = list(x1.var)
+    for v in x2.var:
         if v not in var_list:
             var_list.append(v)
     var_list.sort()
-    shape = get_shape(f1.var, var_list)
-    f1.array.resize(shape)
-    shape = get_shape(f2.var, var_list)
-    f2.array.resize(shape)
-    return Factor(f1.array * f2.array, var_list)
+    shape = get_shape(x1.var, var_list)
+    x1.array.resize(shape)
+    shape = get_shape(x2.var, var_list)
+    x2.array.resize(shape)
+    return Factor(x1.array * x2.array, var_list)
 
 
 def get_shape(var, combined_var):
@@ -63,14 +65,15 @@ def inference(factorList, queryVariables, orderedListOfHiddenVariables, evidence
     nfl = list()
     if type(evidenceList) == dict:
         for f in factorList:
-            tmp = Factor()
+            tmp = copy.deepcopy(f)
             for (k, v) in evidenceList.items():
-                if k in f.var:
-                    tmp = restrict(f, k, v)
-            nfl.append(tmp)
+                if k in tmp.var:
+                    tmp = restrict(tmp, k, v)
+            if tmp:
+                nfl.append(tmp)
     else:
         nfl = factorList
-    ret = nfl[0]
+    ret = copy.deepcopy(nfl[0])
     for i in range(1, len(nfl)):
         ret = multiply(ret, nfl[i])
     for i in orderedListOfHiddenVariables:
@@ -81,53 +84,49 @@ def inference(factorList, queryVariables, orderedListOfHiddenVariables, evidence
     return ret
 
 if __name__ == "__main__":
-    order = ['Trav', 'FP', 'Fraud', 'IP', 'OC', 'CRP']
+    order = ['Trav', 'Fp', 'Fraud', 'Ip', 'OC', 'Crp']
+    # f1(OC) = Pr(OC)
+    f1 = Factor([0.8, 0.2], ['OC'])
+    # f2(Trav) = Pr(Trav)
+    f2 = Factor([0.05, 0.95], ['Trav'])
+    # f3(Fraud, Trac) = Pr(Fraud|Trav)
+    f3 = Factor([[0.01, 0.004], [0.99, 0.996]], ['Fraud', 'Trav'])
+    # f4(Fp, Trav, Fraud) = Pr(Fp|Trav, Fraud)
+    f4 = Factor([[[0.9, 0.1], [0.9, 0.01]], [[0.1, 0.9], [0.1, 0.99]]], ['Fp', 'Fraud', 'Trav'])
+    # f5(Ip, OC, Fraud) = Pr(Ip|OC, Fraud)
+    f5 = Factor([[[0.15, 0.051], [0.85, 0.949]], [[0.1, 0.001], [0.9, 0.999]]], ['Fraud', 'Ip', 'OC'])
+    # f6(Crp, OC) = Pr(Crp|OC)
+    f6 = Factor([[0.1, 0.01], [0.9, 0.99]], ['Crp', 'OC'])
+
+    f_list = [f1, f2, f3, f4, f5, f6]
 
     #q2 (b) Pr(Fraud)
-    f_Tr = Factor([0.05, 0.95], ['Trav'])
-    f_Fr_Tr = Factor([[0.01, 0.004], [0.99, 0.996]], ['Fraud', 'Trav'])
-    x = inference([f_Tr, f_Fr_Tr], 'Fraud', ['Trav'])
-    print(x.array, x.var)
+    print 'Pr(Fraud)'
+    h_order = ['Trav', 'Fp', 'Ip', 'OC', 'Crp']
+    x = inference(f_list, 'Fraud', h_order)
+    print (x.array, x.var)
 
-    # q2 (b) Pr(Fraud|fp, ~ip, crp)
-    # f1(OC, Fraud) = Pr(~ip|OC, Fraud)
-    f1 = Factor([[0.85, 0.049], [0.9, 0.999]], ['Fraud', 'OC'])
-    # f2(OC) = Pr(crp|OC)
-    f2 = Factor([0.1, 0.01], ['OC'])
-    # f3(OC) = Pr(OC)
-    f3 = Factor([0.8, 0.2], ['OC'])
-    # f4(Trav, Fraud) = Pr(fp|Trav, Fraud)
-    f4 = Factor([[0.9, 0.1], [0.9, 0.01]], ['Fraud', 'Trav'])
-    # f5(Trav) = Pr(Trav)
-    f5 = Factor([0.05, 0.95], ['Trav'])
-    # f6(Fraud) = Pr(Fraud)
-    f6 = Factor([0.0043, 0.9957], ['Fraud'])
-    x = inference([f1,f2,f3,f4,f5,f6], 'Fraud', ['Trav', 'OC'])
-    print(x.array, x.var)
+    #q2 (b) Pr(Fraud|fp, ~ip, crp)
+    print 'Pr(Fraud|fp, ~ip, crp)'
+    h_order = ['Trav', 'OC']
+    x = inference(f_list, 'Fraud', h_order, {'Fp': True, 'Ip': False, 'Crp': True})
+    print (x.array, x.var)
 
     # q2 (c) Pr(Fraud|fp, ~ip, crp, trav)
-    # f1(OC, Fraud) = Pr(~ip|OC, Fraud)
-    f1 = Factor([[0.85, 0.049], [0.9, 0.999]], ['Fraud', 'OC'])
-    # f2(OC) = Pr(crp|OC)
-    f2 = Factor([0.1, 0.01], ['OC'])
-    # f3(OC) = Pr(OC)
-    f3 = Factor([0.8, 0.2], ['OC'])
-    # f4(Fraud) = Pr(fp|trav, Fraud)
-    f4 = Factor([[0.9, 0.9]], ['Fraud'])
-    # f6(Fraud) = Pr(Fraud)
-    f6 = Factor([0.0043, 0.9957], ['Fraud'])
-    x = inference([f1, f2, f3, f4, f6], 'Fraud', ['OC'])
-    print(x.array, x.var)
+    print 'Pr(Fraud|fp, ~ip, crp, trav)'
+    h_order = ['OC']
+    x = inference(f_list, 'Fraud', h_order, {'Fp': True, 'Ip': False, 'Crp': True, 'Trav': True})
+    print (x.array, x.var)
 
+    # q2 (d)
+    # Pr(Fraud|ip)
+    print 'Pr(Fraud|ip)'
+    h_order = ['Trav', 'Fp', 'OC', 'Crp']
+    x = inference(f_list, 'Fraud', h_order, {'Ip': True})
+    print (x.array, x.var)
 
-    # q2 (d) Pr(Fraud|ip)
-    # f1(OC, Fraud) = Pr(ip|Fraud)
-    f1 = Factor([[0.15, 0.051], [0.1, 0.001]], ['Fraud', 'OC'])
-    # f2(Fraud) = Pr(Fraud)
-    f2 = Factor([0.0043, 0.9957], ['Fraud'])
-    x = inference([f1, f2], 'Fraud', ['OC'])
-    print(x.array, x.var)
-    # f3(OC) = Pr(crp|OC)
-    f3 = Factor([0.1, 0.01], ['OC'])
-    x = inference([f1, f2, f3], 'Fraud', ['OC'])
-    print(x.array, x.var)
+    # Pr(Fraud|ip, crp)
+    print 'Pr(Fraud|ip, crp)'
+    h_order = ['Trav', 'Fp', 'OC']
+    x = inference(f_list, 'Fraud', h_order, {'Ip': True, 'Crp': True})
+    print (x.array, x.var)
