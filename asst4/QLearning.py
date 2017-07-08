a29cdc1c545e5539f5fb228e3deacc8cbbdfb83b
Erlie Shang <erlie.shang@uwaterloo.ca>
2017-07-07 23:06:02 -0400

    Q2

--- asst4/QLearning.py
+++ asst4/QLearning.py
@@ -16,7 +16,7 @@ def select_action(actions):
     return ret
 
 
-def next_state(state, action, t):
+def next_state(state, action, t, g):
     p = [0.0 for i in range(17)]
     for i in range(17):
         p[i] = t[state, i, action]
@@ -24,18 +24,38 @@ def next_state(state, action, t):
     for i in range(17):
         x -= p[i]
         if x <= 0:
-            return i
+            if random.random() > g:
+                return i
+            else:
+                return random.randint(0, 3)
     return None
 
 
-def main():
+def q_learning(greedy):
     t, r = gridWorld(0.9, 0.05)
-    a = 0.5
-    q_table = [[0, 0, 0, 0] for i in range(17)]
-    # start from state 4
-    action = select_action(q_table[4])
-    ns = next_state(4, action, t)
-    q_table[4][action] += a * (r[4] + FACTOR * q_table[ns][select_action(q_table[ns])] - q_table[4][action])
+    q_table = [[0.0, 0.0, 0.0, 0.0] for i in range(17)]
+    q_counter = [[0.0, 0.0, 0.0, 0.0] for i in range(17)]
+    for i in range(10000):
+        state = 4
+        while state != 16:
+            action = select_action(q_table[state])
+            ns = next_state(state, action, t, greedy)
+            q_counter[state][action] += 1
+            q_table[state][action] += ((1 / q_counter[state][action]) * (
+                r[state] + FACTOR * q_table[ns][select_action(q_table[ns])] - q_table[state][action]))
+            state = ns
+    return q_table
+
+
+def main():
+    q1 = q_learning(0.05)
+    print "when greedy = 0.05"
+    for i in range(17):
+        print "state", i, "action:", actions[select_action(q1[i])], "Q value:", q1[i][select_action(q1[i])]
+    q2 = q_learning(0.2)
+    print "when greedy = 0.2"
+    for i in range(17):
+        print "state", i, "action:", actions[select_action(q2[i])], "Q value:", q2[i][select_action(q2[i])]
 
 
 if __name__ == "__main__":
