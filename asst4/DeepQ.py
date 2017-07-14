ca3022f929f7d4bf7bf739d25b2331c5ad815770
Erlie Shang <erlie.shang@uwaterloo.ca>
2017-07-14 10:33:33 -0400

    A4 finished

--- asst4/DeepQ.py
+++ asst4/DeepQ.py
@@ -1,138 +1,219 @@
 # Author: erlie.shang@uwaterloo.ca
-
-import gym
-import os
-import tensorflow as tf
-from collections import deque
 import random
+import gym
 import numpy as np
+import matplotlib.pyplot as plt
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 
 
 class PoleAgent(object):
     def __init__(self):
-        self.buffer = deque()
-        self.buffer_size = 1000
-        self.factor = 0.99
-        self.learning_rate = 0.01
+        self.state_size = 4
+        self.action_size = 2
+        self.buffer = deque(maxlen=1000)
+        self.batch_size = 50
+        self.discount = 0.99
         self.greedy = 0.05
-        self._build_model()
-        self.sess = tf.Session()
-        self.sess.run(tf.global_variables_initializer())
-
-    def save(self):
-        saver = tf.train.Saver()
-        save_path = saver.save(self.sess, "./tmp/model.ckpt")
-        print("Model saved in file: %s" % save_path)
-
-    def load(self):
-        if os.path.isfile('./tmp/model.ckpt'):
-            saver = tf.train.Saver()
-            saver.restore(self.sess, "./tmp/model.ckpt")
-            print("Model restored.")
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+        self.target = self._build_model()
 
     def _build_model(self):
-        self.input = tf.placeholder(tf.float32, [None, 4], name='input')
-        self.q_target = tf.placeholder(tf.float32, [None, 2], name='Q_target')
+        model = Sequential()
+        model.add(Dense(10, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(10, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mean_squared_error', optimizer=Adam(lr=self.learning_rate))
+        return model
 
-        self.w1 = tf.Variable(tf.random_uniform([4, 10], minval=-1.0, maxval=1.0))
-        self.b1 = tf.Variable(tf.random_uniform([10], minval=-1.0, maxval=1.0))
-        self.l1 = tf.nn.relu(tf.matmul(self.input, self.w1) + self.b1)
-        self.w2 = tf.Variable(tf.random_uniform([10, 10], minval=-1.0, maxval=1.0))
-        self.b2 = tf.Variable(tf.random_uniform([10], minval=-1.0, maxval=1.0))
-        self.l2 = tf.nn.relu(tf.matmul(self.l1, self.w2) + self.b2)
-        self.w3 = tf.Variable(tf.random_uniform([10, 2], minval=-1.0, maxval=1.0))
-        self.b3 = tf.Variable(tf.random_uniform([2], minval=-1.0, maxval=1.0))
-        self.output = tf.matmul(self.l2, self.w3) + self.b3
+    def add(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
 
-        self.t_input = tf.placeholder(tf.float32, [None, 4], name='t_input')
-        self.t_w1 = tf.Variable(tf.random_uniform([4, 10], minval=-1.0, maxval=1.0))
-        self.t_b1 = tf.Variable(tf.random_uniform([10], minval=-1.0, maxval=1.0))
-        self.t_l1 = tf.nn.relu(tf.matmul(self.t_input, self.t_w1) + self.t_b1)
-        self.t_w2 = tf.Variable(tf.random_uniform([10, 10], minval=-1.0, maxval=1.0))
-        self.t_b2 = tf.Variable(tf.random_uniform([10], minval=-1.0, maxval=1.0))
-        self.t_l2 = tf.nn.relu(tf.matmul(self.t_l1, self.t_w2) + self.t_b2)
-        self.t_w3 = tf.Variable(tf.random_uniform([10, 2], minval=-1.0, maxval=1.0))
-        self.t_b3 = tf.Variable(tf.random_uniform([2], minval=-1.0, maxval=1.0))
-        self.t_output = tf.matmul(self.t_l2, self.t_w3) + self.t_b3
-
-        self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.output))
-        self.train_step = tf.train.AdagradOptimizer(self.learning_rate).minimize(self.loss)
-
-    def act(self, observation):
+    def act(self, state):
         if random.random() <= self.greedy:
-            return random.randrange(0, 2)
+            return random.randrange(self.action_size)
         else:
-            state = observation.reshape(1, 4)
-            actions = self.sess.run(self.output, feed_dict={self.input: state})
-            return np.argmax(actions)
-
-    def predict(self, observation):
-        state = observation.reshape(1, 4)
-        actions = self.sess.run(self.output, feed_dict={self.input: state})
-        return np.argmax(actions)
-
-    def add(self, state, action, reward, next_state):
-        self.buffer.append((state, action, reward, next_state))
-        if len(self.buffer) > self.buffer_size:
-            self.buffer.popleft()
+            return np.argmax(self.model.predict(state)[0])
 
     def update_target(self):
-        self.t_w1 = tf.assign(self.t_w1, self.w1)
-        self.t_b1 = tf.assign(self.t_b1, self.b1)
-        self.t_w2 = tf.assign(self.t_w2, self.w2)
-        self.t_b2 = tf.assign(self.t_b2, self.b2)
-        self.t_w3 = tf.assign(self.t_w3, self.w3)
-        self.t_b3 = tf.assign(self.t_b3, self.b3)
-        self.t_l1 = tf.nn.relu(tf.matmul(self.t_input, self.t_w1) + self.t_b1)
-        self.t_l2 = tf.nn.relu(tf.matmul(self.t_l1, self.t_w2) + self.t_b2)
-        self.t_output = tf.matmul(self.t_l2, self.t_w3) + self.t_b3
-        print 'target updated'
+        self.target.set_weights(self.model.get_weights())
 
-    def fix_shape(self, nparray):
-        ret = [[0, 0, 0, 0] for _ in range(nparray.size)]
-        for i in range(nparray.size):
-            for j in range(4):
-                ret[i][j] = nparray[i][j]
-        return np.array(ret)
+    def train_0(self, state, action, reward, next_state, done):
+        target = self.model.predict(state)
+        target[0][action] = reward if done else reward + self.discount * np.amax(self.model.predict(next_state)[0])
+        self.model.fit(state, target, epochs=1, verbose=0)
 
-    def train(self, batch_size=50):
-        mini_batch = random.sample(self.buffer, batch_size)
-        mini_batch = np.array(mini_batch)
-        state = mini_batch[:, 0]
-        next_state = mini_batch[:, 3]
-        state = self.fix_shape(state)
-        next_state = self.fix_shape(next_state)
-        t_output, output = self.sess.run([self.t_output, self.output], {self.t_input: next_state, self.input: state, })
-        q_target = output.copy()
-        index = range(batch_size)
-        actions = mini_batch[:, 1].astype(int)
-        rewards = mini_batch[:, 2]
-        q_target[index, actions] = rewards + self.factor * np.max(t_output, axis=1)
+    def train_1(self):
+        mini_batch = random.sample(self.buffer, self.batch_size)
+        states = list()
+        actions = list()
+        rewards = list()
+        for state, action, reward, next_state, done in mini_batch:
+            states.append(state[0])
+            actions.append(int(action))
+            rewards.append(reward if done else reward + self.discount * np.amax(self.model.predict(next_state)[0]))
+        states = np.array(states)
+        actions = np.array(actions)
+        rewards = np.array(rewards)
+        target = self.model.predict(states, batch_size=self.batch_size)
+        index = range(self.batch_size)
+        target[index, actions] = rewards
+        self.model.fit(states, target, batch_size=self.batch_size, epochs=1, verbose=0)
 
-        self.sess.run(self.train_step, feed_dict={self.input: state, self.q_target: q_target})
+    def train_2(self, state, action, reward, next_state, done):
+        target = self.model.predict(state)
+        target[0][action] = reward if done else reward + self.discount * np.amax(self.target.predict(next_state)[0])
+        self.model.fit(state, target, epochs=1, verbose=0)
+
+    def train_3(self):
+        mini_batch = random.sample(self.buffer, self.batch_size)
+        states = list()
+        actions = list()
+        rewards = list()
+        for state, action, reward, next_state, done in mini_batch:
+            states.append(state[0])
+            actions.append(int(action))
+            rewards.append(reward if done else reward + self.discount * np.amax(self.target.predict(next_state)[0]))
+        states = np.array(states)
+        actions = np.array(actions)
+        rewards = np.array(rewards)
+        target = self.model.predict(states, batch_size=self.batch_size)
+        index = range(self.batch_size)
+        target[index, actions] = rewards
+        self.model.fit(states, target, batch_size=self.batch_size, epochs=1, verbose=0)
 
 
-def main():
-    env = gym.make('CartPole-v0')
-    agent = PoleAgent()
-    episodes = 1000
-
-    for e in range(episodes):
-        state = env.reset()
-        while True:
+def mode0():
+    # no experience replay and no target network
+    agent0 = PoleAgent()
+    survival_steps = list()
+    for e in range(EPISODES):
+        state = env.reset().reshape(1, 4)
+        for step in range(500):
             env.render()
-            action = agent.act(state)
-            next_state, reward, done, info = env.step(action)
-            agent.add(state, action, reward, next_state)
+            action = agent0.act(state)
+            next_state, reward, done, _ = env.step(action)
+            next_state = next_state.reshape(1, 4)
+            reward = reward if not done else 0
+            agent0.train_0(state, action, reward, next_state, done)
             state = next_state
-            if len(agent.buffer) > 100:
-                agent.train()
             if done:
+                print("episode: {}/{}, step: {}".format(e + 1, EPISODES, step))
+                survival_steps.append(step)
                 break
-        print("episode: {}/{}".format(e + 1, episodes))
-        if (e + 1) % 2 == 0:
-            agent.update_target()
+    plt.plot(range(1, EPISODES+1), survival_steps, linewidth=2.5)
+    plt.title('No experience replay and no target network')
+    plt.xlabel('Episodes')
+    plt.ylabel('survival_steps')
+    plt.grid()
+    plt.savefig('mode_0.png')
+    plt.close('all')
+
+
+def mode1():
+    # no target network
+    agent1 = PoleAgent()
+    survival_steps = list()
+    for e in range(EPISODES):
+        state = env.reset().reshape(1, 4)
+        for step in range(500):
+            env.render()
+            action = agent1.act(state)
+            next_state, reward, done, _ = env.step(action)
+            next_state = next_state.reshape(1, 4)
+            reward = reward if not done else 0
+            agent1.add(state, action, reward, next_state, done)
+            if len(agent1.buffer) > agent1.batch_size:
+                agent1.train_1()
+            state = next_state
+            if done:
+                print("episode: {}/{}, step: {}".format(e + 1, EPISODES, step))
+                survival_steps.append(step)
+                break
+    plt.plot(range(1, EPISODES+1), survival_steps, linewidth=2.5)
+    plt.title('Experience Replay Enabled')
+    plt.xlabel('Episodes')
+    plt.ylabel('survival_steps')
+    plt.grid()
+    plt.savefig('mode_1.png')
+    plt.close('all')
+
+
+def mode2():
+    # no experience replay
+    agent2 = PoleAgent()
+    survival_steps = list()
+    for e in range(EPISODES):
+        state = env.reset().reshape(1, 4)
+        for step in range(500):
+            env.render()
+            action = agent2.act(state)
+            next_state, reward, done, _ = env.step(action)
+            next_state = next_state.reshape(1, 4)
+            reward = reward if not done else 0
+            agent2.train_2(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                print("episode: {}/{}, step: {}".format(e + 1, EPISODES, step))
+                survival_steps.append(step)
+                break
+        if e % 2 == 1:
+            agent2.update_target()
+    plt.plot(range(1, EPISODES+1), survival_steps, linewidth=2.5)
+    plt.title('Target Network Enabled')
+    plt.xlabel('Episodes')
+    plt.ylabel('survival_steps')
+    plt.grid()
+    plt.savefig('mode_2.png')
+    plt.close('all')
+
+
+def mode3():
+    # both enabled
+    agent3 = PoleAgent()
+    survival_steps = list()
+    for e in range(EPISODES):
+        state = env.reset().reshape(1, 4)
+        for step in range(500):
+            env.render()
+            action = agent3.act(state)
+            next_state, reward, done, _ = env.step(action)
+            next_state = next_state.reshape(1, 4)
+            reward = reward if not done else 0
+            agent3.add(state, action, reward, next_state, done)
+            if len(agent3.buffer) > agent3.batch_size:
+                agent3.train_3()
+            state = next_state
+            if done:
+                print("episode: {}/{}, step: {}".format(e + 1, EPISODES, step))
+                survival_steps.append(step)
+                break
+        if e % 2 == 1:
+            agent3.update_target()
+    plt.plot(range(1, EPISODES+1), survival_steps, linewidth=2.5)
+    plt.title('Experience Replay and Target Network Enabled')
+    plt.xlabel('Episodes')
+    plt.ylabel('survival_steps')
+    plt.grid()
+    plt.savefig('mode_3.png')
+    plt.close('all')
 
 
 if __name__ == "__main__":
-    main()
+    EPISODES = 1000
+    env = gym.make('CartPole-v1')
+    mode0()
+    mode1()
+    mode2()
+    mode3()
+
+
+
+
+
+
+
+
